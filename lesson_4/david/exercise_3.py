030fa0e5663a1d76eea7ca7d9bd5f99117b74736
davidhasenson <90691386+davidhasenson@users.noreply.github.com>
2022-09-05 15:03:16 +0200

    nya filer

---
+++ lesson_4/david/exercise_3.py
@@ -0,0 +1,9 @@
+# Write a program that fulfills the specification:"
+
+# Ask the user for a word
+word = input("write a word: ")
+
+# Print the word x times, where x = len(word). i.e if the word is hello the program writes:
+x = len(word)
+for x in range (x):
+    print(word)
