838e011aec9f0d12cd3a9b9bb80a47b095475656
davidhasenson <90691386+davidhasenson@users.noreply.github.com>
2022-09-05 15:26:02 +0200

    ny fil

--- lesson_4/david/exercise_5.py
+++ lesson_4/david/exercise_5.py
@@ -1,2 +1,19 @@
 # Exercise 5 Until stop
-#
+# Write a program that fulfills the specification:
+
+# Create a while loop that runs forever
+while 0 < 1:
+
+# In each iteration, input a text
+    word = input("Write something: ")
+
+# In each iteration, print a text "Enter stop to quit: "
+    print("Enter stop to quit. ")
+
+# If the text equals stop, break the while loop
+    if word == "stop":
+        break
+
+# If the text don't equals stop, print the text and the length of the text
+    else:
+        print(f"{word} {len(word)}")
