9f34be3c30421011f497da95f9c74cd09b5720e0
davidhasenson <90691386+davidhasenson@users.noreply.github.com>
2022-09-06 21:55:28 +0200

    ny fil

--- lesson_4/david/extra_exercise_5.py
+++ lesson_4/david/extra_exercise_5.py
@@ -5,29 +5,36 @@
 # second_list = [2, 3, 6, 7, 9]
 # Output: [(2, 3), (3, 0), (6, 4), (7, 1), (9, 2)]
 
-from gettext import find
-
-
 first_list = [3, 7, 9, 2, 6]
 second_list = [2, 3, 6, 7, 9]
 new_list = []
+# print(second_list[1])
+# print(second_list.index(3))
 
+# om x är mindre än längden listan utför loopen
+for x in range(len(second_list)):
+   #print(x)
+   # om värdet i second_list[x] finns i first_list. gå in.
+   if second_list[x] in first_list: 
+        # print(second_list[x]) 
+        # print(first_list[x])
+        
+        # second_list[x] hämtar värdet på position x i second_list. 
+        # first_list.index() hämtar index (platsen i en lista) i first_list för värdet som gavs av second_list[x] 
+        # first_list[] hämtar värdet som finns på index (platsen) som first_list.index() anger
+        get_index = first_list[first_list.index(second_list[x])]
+        # .index breättar positionen i first_list på värdet som anges av second_list[x] ?
+        #eller
+        # second_list[x] hämtar värdet på position x i second_list. 
+        # first_list.index() hämtar index (positionen i en lista) i first_list för värdet som gavs av second_list[x]
+        get_value = first_list.index(second_list[x])
+        # print(get_index)
+        # print(get_value)
 
-for x in second_list:
-   if x == first_list:
-    print(x)
-    new_list.append(x ,y)
-   
+        # skapa en ny tuple med värdena
+        new_tuple = (get_index, get_value)
+        # print(new_tuple)
+
+        # lägg till puple til lista
+        new_list.append(new_tuple)
 print(new_list)
-   
-   
-"""" for y in second_list:
-        a = (x, y)
-        combined_list.append(a)
-   #     y =+1
-  #      z =+1
-        #print(x, y )
-     
-        break
-print(combined_list)
-"""
