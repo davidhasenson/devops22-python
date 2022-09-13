1fba716609136338f5900dcd0437a263fd6e5c2c
davidhasenson <90691386+davidhasenson@users.noreply.github.com>
2022-09-13 16:11:49 +0200

    mer

---
+++ lesson_7/davidhasenson/extra_exercise_3.py
@@ -0,0 +1,26 @@
+def num_list(my_list=[]):
+    return my_list
+
+#def list_name(A, B):
+#    return A=num_list[0] , B=num_list[1]
+
+def bigger(A, B):
+    if A > B:
+        return True
+    else:
+        False
+
+def swap_place(A, B):
+    return A,B == B, A 
+
+def check_list_end(list_index, my_list):
+    if list_index == len(my_list):
+        return True
+    else:
+        return False
+
+def compare_list(list_1, list_2):
+    if list_1 == list_2:
+        return True
+    else:
+        return False
