8b716a44c99dc773a4fca13f3463bc8e83c28620
davidhasenson <90691386+davidhasenson@users.noreply.github.com>
2022-09-13 10:40:25 +0200

    mer

--- lesson_6/davidhasenson/exercise_6_copy.py
+++ lesson_6/davidhasenson/exercise_6_copy.py
@@ -1,6 +1,6 @@
 my_variable = "Hello"
 my_secound_variable = my_variable
-#my_third_variable= my_variable.copy()
+my_third_variable= copy(my_variable)
 my_secound_variable = "World"
 
 print(my_variable)
